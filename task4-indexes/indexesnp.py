dc560803de188ac47b7170f4c6088bd3a555d085
Giovanni <10683631@polimi.it>
2025-01-30 16:22:57 +0100

    Devo implementare i0, i1 come queue cosí che la ricerca diventi un bel O(N)

--- task4-indexes/indexesnp.py
+++ task4-indexes/indexesnp.py
@@ -25,23 +25,27 @@ def count_intervals_opt(N, arr):
     possible_equivalences = compute_indexes(N, arr)
     M = np.zeros((N, N), dtype=int)
     for pos, el in enumerate(tqdm(arr, desc="looking for intervals in the array")):
-        # TODO: direct access to the possible_equivalences pairs to prioritize the closest ones.
         min_triplet_index = np.inf
         for i0, i1 in possible_equivalences[el].values():
+            # TODO: direct access to the possible_equivalences pairs to prioritize the closest ones.
+            # need to check what values are in the next indexes, filtering the ones that re in amici_moltiplicatori[el] e ma anche i divisori?
             i0 = i0[i0 > pos]
             i1 = i1[i1 > pos]
             if i0.size == 0 or i1.size == 0:
                 continue
-            val1_index = i0[0]
-            val2_index = i1[0]
-            if val1_index == val2_index:
+            index_val1 = i0[0]
+            index_val2 = i1[0]
+            if index_val1 == index_val2:
+                assert (
+                    el == arr[index_val1] == arr[index_val2]
+                ), f"Values are not equal, shouldn't be in this branch..."
                 # pick the next one.
                 if i0.size == 1 and i1.size == 1:
                     # there is only one value to be joint and is not possible to create the triplet, skip
                     continue
-                val2_index = i1[1]
-            if min_triplet_index > max(val1_index, val2_index):
-                min_triplet_index = max(val1_index, val2_index)
+                index_val2 = i1[1]
+            if min_triplet_index > max(index_val1, index_val2):
+                min_triplet_index = max(index_val1, index_val2)
             if min_triplet_index == pos + 2:
                 # the best possible case is found, can break the loop for this element
                 break
@@ -55,14 +59,23 @@ def compute_indexes(N, arr):
     indexes = np.arange(N, dtype=int)
     possible_equivalences = {val: {} for val in range(1, max_val + 1)}
     for val in trange(
-        1, max_val + 1, desc="applying roots on index!?"
+        1,
+        max_val + 1,
+        # desc="applying roots on index!?"
     ):  # can this be optimized? what values should we consider?
         val_sq = val**2
         for val2, val3 in amici_moltiplicatori[val_sq]:
-            if val2 <= val3 and val3 <= max_val:  # val3 is int
+            if val2 == val3:
+                assert val == val2 and val == val3
+                i0 = indexes[arr == val]
+                if i0.size == 0 or i1.size == 0 or i2.size == 0:
+                    continue
+                possible_equivalences[val][(val2, val3)] = np.array(
+                    (i0, i0), dtype=object
+                )
+            if val2 < val3:
                 # put in possible_equivalences the indexes of the other values
                 assert val2 * val3 == val_sq, f"{val2} * {val3} != {val_sq}"
-                val3 = int(val3)
                 i0 = indexes[arr == val]
                 i1 = indexes[arr == val2]
                 i2 = indexes[arr == val3]
@@ -82,9 +95,10 @@ def compute_indexes(N, arr):
 
 def count_intervals(N, a):
     "OG solution for debugging purposes only"
+    "to complete task 2 it took?"
     cases = set()
-    for i in range(N):
-        for j in range(i + 1, N):
+    for i in trange(N, desc="elements in the array"):
+        for j in trange(i + 1, N, desc="el2 in the array"):
             for k in range(j + 1, N):
                 if (
                     a[i] * a[j] == a[k] ** 2
